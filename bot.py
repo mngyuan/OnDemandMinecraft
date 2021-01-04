fe107db27ff9a0f523d876d83ba6d4a6d36b4cfe
Kevin Lee <me@mngyuan.com>
2021-01-03 17:38:16 -0800

    fix bot pickling error, hide bot key

--- bot.py
+++ bot.py
@@ -5,16 +5,16 @@ from server_management import manageServer
 import boto3
 
 bot = commands.Bot(command_prefix='>')
+client = boto3.client(
+    'ec2',
+    aws_access_key_id=Config.ACCESS_KEY,
+    aws_secret_access_key=Config.SECRET_KEY,
+    region_name=Config.ec2_region
+)
 
 @bot.command()
 async def start(ctx):
-    client = boto3.client(
-        'ec2',
-        aws_access_key_id=Config.ACCESS_KEY,
-        aws_secret_access_key=Config.SECRET_KEY,
-        region_name=Config.ec2_region
-    )
     message = manageServer(client)
     await ctx.send(message)
 
-bot.run('MzIwMDkzOTA0Mzk5NzYxNDA4.WTEMrQ.pyZG23SPP3B3EQ_WsK56AJu1vog')
+bot.run(Config.DISCORD_BOT_TOKEN)
